6543b3809274341547aeddf4a2c1220a4649a75d
samrudhigawani <samrudhigawani26@gmail.com>
2025-11-11 12:01:58 +0530

    Add age input and voting eligibility check

---
+++ age.py
@@ -0,0 +1,8 @@
+try:
+  age=int(input("enter your age:"))
+  ifa>18:
+  print("you are eligible to vote.")
+else:
+  print("you are not eligible to vote.")
+except ValueError:
+print("please enter a valid integer for age.")
